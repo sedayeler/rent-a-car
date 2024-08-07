a6fb632298aa43754c003bde5f210fc323dcd077
Seda Yeler <sedaayeler@gmail.com>
2024-08-07 18:30:51 +0300

    First Commit

--- DataAccess/Concrete/EntityFramework/RentACarContext.cs
+++ DataAccess/Concrete/EntityFramework/RentACarContext.cs
@@ -18,6 +18,5 @@ namespace DataAccess.Concrete.EntityFramework
         public DbSet<Car> cars { get; set; }
         public DbSet<Brand> brands { get; set; }
         public DbSet<Color> colors { get; set; }
-
     }
 }
